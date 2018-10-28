901be87d7ff3fe66689590f182ba922f4f72cbd5
Agustín Villafañe <agusvillafane@yahoo.com.ar>
2018-10-28 04:36:05 -0300

    Agregando campos
    ... a la inscripcion

---
+++ edu-manage/src/main/resources/db/migration/V0004__agregar_estado_inscripcion.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.inscripciones
+    ADD COLUMN estado_id integer NOT NULL DEFAULT 1;
---
+++ edu-manage/src/main/resources/db/migration/V0005__tipo_inscripcion.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.inscripciones
+    ADD COLUMN tipo character varying(30) COLLATE pg_catalog."es_AR";
