301228cde73ebc892cf4505b6d5739b4df5c27a2
Agustín Villafañe <agusvillafane@yahoo.com.ar>
2020-04-05 20:27:23 -0300

    Eliminadas algunas Warnings!

--- src/main/java/ar/com/strellis/edumanage/dao/GroupDAO.java
+++ src/main/java/ar/com/strellis/edumanage/dao/GroupDAO.java
@@ -3,7 +3,6 @@ package ar.com.strellis.edumanage.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ar.com.strellis.edumanage.model.Group;
--- src/main/java/ar/com/strellis/edumanage/service/impl/CursoServiceImpl.java
+++ src/main/java/ar/com/strellis/edumanage/service/impl/CursoServiceImpl.java
@@ -3,7 +3,6 @@ package ar.com.strellis.edumanage.service.impl;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
