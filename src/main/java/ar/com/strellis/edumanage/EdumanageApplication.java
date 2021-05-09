9d69f26fa9136467b6204695e755c27ac2975f94
Agustin Villafane <agusvillafane@yahoo.com.ar>
2021-05-09 12:41:47 -0300

    Ahora anda tanto en jar como en war dentro de Tomcat 8.5!!

--- src/main/java/ar/com/strellis/edumanage/EdumanageApplication.java
+++ src/main/java/ar/com/strellis/edumanage/EdumanageApplication.java
@@ -2,6 +2,7 @@ package ar.com.strellis.edumanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
@@ -12,5 +13,8 @@ public class EdumanageApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(EdumanageApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EdumanageApplication.class);
+	}
 }
